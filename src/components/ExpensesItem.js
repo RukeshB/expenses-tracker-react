e87b92123042fce73ac69d12c1a910c42413aef6
RukeshB <rukeshbasukala@gmail.com>
2023-05-31 00:10:47 +0545

    display date in human readable format

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,9 +1,16 @@
 import "./ExpensesItem.css"
 
 function ExpensesItem(props) {
+  const month = props.date.toLocaleString('default', { month: 'long'});
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+
   return (
     <div className="expense-item">
-      <div>{ props.date.toDateString() }</div>
+      <div>
+        <div>{ month}</div>
+        <div>{ props.date.getFullYear() }</div>
+        <div>{ day }</div>
+      </div>
       <div className="expense-item__description">
         <h2>{ props.title }</h2>
         <div className="expense-item__price">Rs { props.amount }</div>
