3c65716f3222547c51100cb535a6cf97d91a8614
RukeshB <rukeshbasukala@gmail.com>
2023-05-31 00:17:33 +0545

    make components for expense date

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,19 +1,13 @@
-import "./ExpensesItem.css"
+import "./ExpensesItem.css";
+import ExpesnesDate from "./ExpensesDate";
 
 function ExpensesItem(props) {
-  const month = props.date.toLocaleString('default', { month: 'long'});
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{ month}</div>
-        <div>{ props.date.getFullYear() }</div>
-        <div>{ day }</div>
-      </div>
+      <ExpesnesDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{ props.title }</h2>
-        <div className="expense-item__price">Rs { props.amount }</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs {props.amount}</div>
       </div>
     </div>
   );
